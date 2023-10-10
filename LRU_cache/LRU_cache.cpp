d548c69bca69e78adf044e4605a3f86ac4cc13f0
igors <igors@CLEVO>
2023-10-10 22:54:15 +0300

    Добавьте файлы проекта.

---
+++ LRU_cache/LRU_cache.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include <unordered_map>
+#include <list>
+using namespace std;
+
+class LRU_cache {
+private:
+	int capacity;
+	list<pair<int, int>> list;
+	unordered_map<int, std::list<std::pair<int, int>>::iterator> map;
+public:
+	
+
+	int put(int key, int value) {
+		list.push_front(make_pair(key, value));
+	}
+	
+};
+
+int main() {
+	LRU_cache myLRU;
+	cout << myLRU.put(2323, 214);
+}
