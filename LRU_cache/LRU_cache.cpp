6c300e1c2cd2be64228dddc409ca244f5799a262
Shahunov Igor <igorshahunov2408@gmail.com>
2023-10-11 16:17:14 +0300

    Write base struct for class LRUCache

--- LRU_cache/LRU_cache.cpp
+++ LRU_cache/LRU_cache.cpp
@@ -1,23 +1,37 @@
-﻿#include<iostream>
+﻿#include <iostream>
 #include <unordered_map>
 #include <list>
-using namespace std;
 
-class LRU_cache {
-private:
-	int capacity;
-	list<pair<int, int>> list;
-	unordered_map<int, std::list<std::pair<int, int>>::iterator> map;
+
+class LRUCache {
 public:
-	
+    LRUCache(int capacity) {
+        capacity_ = capacity;
+    }
 
-	int put(int key, int value) {
-		list.push_front(make_pair(key, value));
-	}
-	
+    int get(int key) {
+        auto item = cache_.find(key);
+        // if key doesn't exist we return -1
+        if (item == cache_.end())
+        {
+            return -1;
+        }
+        return item->second.first;
+        // if key exist we return value 
+    }
+
+    void put(int key, int value) {
+
+    }
+
+    void use() {
+        
+    }
+
+    int capacity_;
+    // < key < value, key iterator> >for some reason
+    std::unordered_map<int, std::pair<int, std::list<int>::iterator>> cache_;
+    // List of keys 1st is most used, last i lessed used
+    std::list<int> lru_;
 };
 
-int main() {
-	LRU_cache myLRU;
-	cout << myLRU.put(2323, 214);
-}
